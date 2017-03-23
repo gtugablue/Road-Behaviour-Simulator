4f88f549fa217e820d5079c9c1e255756c1668d1
Gustavo Silva <silva95gustavo@gmail.com>
2017-03-23 19:29:30 +0000

    Fixed image size

--- src/js/main.js
+++ src/js/main.js
@@ -18,19 +18,8 @@ $( document ).ready(function() {
     });
 	
 	// Stop sign
-	fabric.Image.fromURL('assets/img/signs/stop.png', function(oImg) {
-            canvas.add(oImg);
-        });
-	
-	// create a rectangle object
-	var rect = new fabric.Rect({
-	  left: 100,
-	  top: 100,
-	  fill: 'red',
-	  width: 20,
-	  height: 20
+	fabric.Image.fromURL('assets/img/signs/stop.png', function(image) {
+			image.scaleToWidth(50);
+			canvas.add(image);
 	});
-
-	// "add" rectangle onto canvas
-	canvas.add(rect);
 });

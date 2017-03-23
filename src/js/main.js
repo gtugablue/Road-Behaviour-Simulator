f166e5f0b5684194e8a8859f374ab5b9dfe3b023
Gustavo Silva <silva95gustavo@gmail.com>
2017-03-23 19:18:12 +0000

    Fixed background async issue

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,20 @@
 $( document ).ready(function() {
 	// create a wrapper around native canvas element (with id="c")
 	var canvas = new fabric.Canvas('c');
-	canvas.setBackgroundImage('https://maps.googleapis.com/maps/api/streetview?size=400x400&location=40.720032,-73.988354&fov=90&heading=235&pitch=10&key=AIzaSyB_DzzYoHNMdyJYe53zW5j81EqRwv7r3RY');
-	canvas.setHeight(400);
-	canvas.setWidth(400);
+	canvas.setWidth(800);
+	canvas.setHeight(600);
+		
+	fabric.Image.fromURL('https://maps.googleapis.com/maps/api/streetview?size=800x600&location=40.720032,-73.988354&fov=90&heading=235&pitch=10&key=AIzaSyB_DzzYoHNMdyJYe53zW5j81EqRwv7r3RY', function(image) {
+        image.set({
+            // I need this because the image size and the canvas size could be different
+            // in this way the image always covers the canvas
+            width:800,
+            height:600,
+        });
+
+       	canvas.setBackgroundImage(image);
+        canvas.renderAll();
+    });
 	
 	// create a rectangle object
 	var rect = new fabric.Rect({

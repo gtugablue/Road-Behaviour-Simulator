29628e090448960c21c3f58d0293626533cac0f0
andrelago13 <andrelago1995@gmail.com>
2017-03-30 19:57:18 +0100

    Started database files

---
+++ src/database/connection.php
@@ -0,0 +1,2 @@
+<?php
+defined ( "DATABASE_PATH" ) or define ( "DATABASE_PATH", str_replace ( '\\', '/', realpath ( $dir_file . '/database' ) ) );
---
+++ src/database/db.sql
@@ -0,0 +1,7 @@
+create table users
+(
+  id int not null auto_increment
+    primary key
+)
+;
+

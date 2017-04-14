6c6eba6d7ef70aebf0ba9a6fa3fe30115efcbd5b
andrelago13 <andrelago1995@gmail.com>
2017-04-14 15:11:54 +0100

    Dashboard template

--- src/routes/dashboard.js
+++ src/routes/dashboard.js
@@ -16,14 +16,32 @@ router.get('/', function (req, res, next) {
         new BreadcrumbItem("Dashboard")
     ];
 
+    var quizes = [
+        new QuizItem(0, "Lorem ipsum FTW", "", 10),
+        new QuizItem(1, "Watch out for that sign!", "", 55),
+        new QuizItem(2, "Erasmus TOP10 destinations", "", 100),
+        new QuizItem(3, "Node.js is awesome!", "", 6),
+        new QuizItem(4, "All hail LIACC", "", 78),
+        new QuizItem(5, "Best course: MSSI for sure!", "", 99999)
+    ];
+
     res.render('dashboard', {
         layout: 'layout',
         title: config.app_title,
         includeCustomStyle: true,
         breadcrumb: page_breadcrumb,
         user_name: req.user.displayName,
-        user_firstname: req.user.displayName.split(" ")[0]
+        user_firstname: req.user.displayName.split(" ")[0],
+        customStyles: ["dashboard"],
+        quizes: quizes
     });
 });
+
+var QuizItem = function (number, title, href, replies) {
+    this.number = number;
+    this.title = title;
+    this.replies = replies;
+    this.href = href;
+};
 
 module.exports = router;

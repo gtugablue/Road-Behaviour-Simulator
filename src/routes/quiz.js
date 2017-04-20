1fd21c4aa3df364fae5284cc6c2d5c1707dac184
andrelago13 <andrelago1995@gmail.com>
2017-04-20 19:52:13 +0100

    started quiz admin page

--- src/routes/quiz.js
+++ src/routes/quiz.js
@@ -1,9 +1,30 @@
 var express = require('express');
 var router = express.Router();
+var config = require('./../configuration/config');
+var BreadcrumbItem = require('./../utils/breadcrumb').BreadcrumbItem;
 
 /* GET question page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express', layout: 'quiz' });
+router.get('/', function (req, res, next) {
+
+  // if is quiz owner
+  var page_breadcrumb = [
+    new BreadcrumbItem("Home", "/"),
+    new BreadcrumbItem("Dashboard", "/dashboard"),
+    new BreadcrumbItem("Quiz")
+  ];
+
+  var quiz = {
+    title: "Placeholder Quizz Name",
+    url: "www.google.pt"
+  };
+
+  res.render('quiz-page', {
+    layout: 'layout',
+    title: config.app_title,
+    includeCustomStyle: true,
+    breadcrumb: page_breadcrumb,
+    quiz: quiz
+  });
 });
 
 module.exports = router;
